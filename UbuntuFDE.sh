a0e123417e7634d57447456c196333827e8093aa
Fab <fab@zenayastudios.com>
2025-04-08 19:08:56 +0300

    update
    moved check_root(

--- UbuntuFDE.sh
+++ UbuntuFDE.sh
@@ -6,6 +6,15 @@
 # Autor: Smali Tobules
 
 
+# Überprüfe die Ausführung mit erhohten Rechten
+check_root() {
+    if [ "$(id -u)" -ne 0 ]; then
+        echo -e "${YELLOW}[HINWEIS]${NC} Dieses Skript benötigt Root-Rechte. Starte neu mit sudo..."
+        exec sudo "$0" "$@"  # Starte das Skript neu mit sudo und behalte alle Argumente bei
+    fi
+}
+
+
 ###################
 # Konfiguration   #
 SCRIPT_VERSION="0.0.1"
@@ -144,13 +153,7 @@ pkg_autoremove() {
 
 ###################
 #   Systemcheck   #
-check_root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        echo -e "${YELLOW}[HINWEIS]${NC} Dieses Skript benötigt Root-Rechte. Starte neu mit sudo..."
-        exec sudo "$0" "$@"  # Starte das Skript neu mit sudo und behalte alle Argumente bei
-    fi
-}
-
+# Abhängigkeiten prüfen und installieren
 check_dependencies() {
     log_info "Prüfe Abhängigkeiten..."
     
