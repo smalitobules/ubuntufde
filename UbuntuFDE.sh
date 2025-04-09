f21b55302faee177497fb48e047a6d486ebcfc1c
Fab <fab@zenayastudios.com>
2025-04-09 04:59:30 +0300

    update
    set another timeout for post-install-settings.service

--- UbuntuFDE.sh
+++ UbuntuFDE.sh
@@ -3166,7 +3166,7 @@ Conflicts=rescue.service rescue.target
 Type=oneshot
 ExecStart=/usr/local/bin/post_install_settings.sh
 RemainAfterExit=yes
-Timeout=30
+TimeoutStartSec=180
 
 [Install]
 WantedBy=multi-user.target
